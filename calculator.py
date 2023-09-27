9bfc13172b1981a23bc02b5957b25b9ee98705c2
FriedOkra1 <contactayushmadhav@gmail.com>
2023-09-27 20:10:23 +0100

    Adding calculator to repo.

---
+++ calculator.py
@@ -0,0 +1,11 @@
+x = float(input(" Type in the value of 'x' to be divided by the number below "))
+y = float(input(" Type in the value of 'y' to be divided from the number above "))
+
+z = round(x/y, 2)
+
+print(f"{z:,}")
+
+a = float(input("Type in the value of 'a' to be multiplied to the number below "))
+b = float(input("Type in the value of 'b' to be multiplied to the number above "))
+c = round(a*b, 2)
+print(f"{c:,}")
